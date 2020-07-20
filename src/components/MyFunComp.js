3f3f152c6be8f78aa40cb61de6a9a9ea37f55457
zhouchao-z <1051386008@qq.com>
2020-07-20 17:02:00 +0800

    render(picker)

--- src/components/MyFunComp.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-export default function MyFunComp(props) {
-  return <h1>函数组件, number为{props.number}</h1>
-}
--- src/index.js
+++ src/index.js
@@ -1,59 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import src1 from './assets/1.jpg';
-// import src2 from './assets/2.jpg';
-// import src3 from './assets/3.jpg';
-// import './assets/index.css'
+import src1 from './assets/1.jpg';
+import src2 from './assets/2.jpg';
+import src3 from './assets/3.jpg';
+import './assets/index.css'
 
-import MyFunComp from './components/MyFunComp.js';
-import MyClassComp from './components/MyClassComp.js';
 
-const obj = {
-  name: 'zc',
-  age: 18
+const srcArr = [src1, src2, src3];
+
+const container = document.getElementById('root');
+
+
+let index = 0;
+let timer;
+function render() {
+  ReactDOM.render(<img src={ srcArr[index] } alt=""/>, document.getElementById('root'));
+}
+
+function start() {
+  stop();
+  timer = setInterval(() => {
+    index = (index + 1) % 3;
+    render();
+  }, 2000);
+}
+render();
+start();
+
+function stop() {
+  clearInterval(timer);
+}
+
+container.onmouseenter = function() {
+  stop();
 };
-ReactDOM.render(
-    <div>
-      <MyFunComp number={3}/>
-      {/*<MyClassComp number={8} test/>*/}
-      {/*<MyClassComp ui={<p>这是我传递的p元素</p>}/>*/}
-      {/*<MyClassComp ui2={<p>这是我传递的第二个p元素</p>}/>*/}
-      <MyClassComp obj={ obj } test={2}/>
-    </div>,
-    document.getElementById('root')
-);
-// const srcArr = [src1, src2, src3];
-//
-// const container = document.getElementById('root');
-//
-//
-// let index = 0;
-// let timer;
-// function render() {
-//   ReactDOM.render(<img src={ srcArr[index] } alt=""/>, document.getElementById('root'));
-// }
-//
-// function start() {
-//   stop();
-//   timer = setInterval(() => {
-//     index = (index + 1) % 3;
-//     render();
-//   }, 2000);
-// }
-// render();
-// start();
-//
-// function stop() {
-//   clearInterval(timer);
-// }
-//
-// container.onmouseenter = function() {
-//   stop();
-// };
-//
-// container.onmouseleave = function() {
-//   start();
-// };
+
+container.onmouseleave = function() {
+  start();
+};
 
 
 
